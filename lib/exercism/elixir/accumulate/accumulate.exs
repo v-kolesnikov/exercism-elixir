7ad1e08a473fdd5e40abc58c3ba600396a90dd6f
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-10-01 10:12:30 +0300

    Add 'accumulate' exercise solution.

---
+++ lib/exercism/elixir/accumulate/accumulate.exs
@@ -0,0 +1,48 @@
+defmodule Accumulate do
+  @moduledoc"""
+  Implement the `accumulate` operation, which, given a collection and an operation to perform on each element of the collection, returns a new collection containing the result of applying that operation to each element of the input collection.
+
+  Given the collection of numbers:
+
+  - 1, 2, 3, 4, 5
+
+  And the operation:
+
+  - square a number (`x => x * x`)
+
+  Your code should be able to produce the collection of squares:
+
+  - 1, 4, 9, 16, 25
+
+  Check out the test suite to see the expected function signature.
+  """
+
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+    accumulate(list, [], fun)
+  end
+
+  defp accumulate([head | tail], acc, f) do
+    accumulate(tail, [f.(head) | acc], f)
+  end
+
+  defp accumulate([], acc, _) do
+    acc |> Enum.reverse
+  end
+end
